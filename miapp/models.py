f189282666b4b30346fd3e72605229e446781c67
Jorge <2016100125@untels.edu.pe>
2021-02-17 11:47:01 -0500

    CRUD Empleados- Listo

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=150)
+    image = models.ImageField(default='null')
+    cases = models.DateTimeField(auto_now_add=True)
+    deaths = models.CharField(max_length=50)
+    lethality = models.CharField(max_length=50)
+
+
--- miapp/views.py
+++ miapp/views.py
@@ -1,14 +1,51 @@
-from django.shortcuts import render, HttpResponse,redirect
+from django.shortcuts import render , HttpResponse, redirect
+from miapp.models import Region
+from django.db.models import Q
+from miapp.forms import FormRegion
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def regiones(request):
-    return render(request,'regiones.html')
+    
+    
+    regiones = Region.objects.all()
+
+    return render(request, 'regiones.html',{
+        'regiones': regiones,
+        'titulo': 'Listado de Regiones'
+    })
 
 def crear_region(request):
-    return render(request,'crear_region.html')
+    if request.method == 'POST':
+        formulario = FormRegion(request.POST)
+        if formulario.is_valid():
+            data_form = formulario.cleaned_data
+            name = data_form.get('name')
+            cases = data_form['cases']
+            deaths = data_form['deaths']
+            lethality = data_form['lethality']
+            
+            region = Region(
+                name = name,
+                cases = cases,
+                deaths = deaths,
+                lethality = lethality
+            )
+            region.save()
+
+            #Es para crear un mensaje Flash (Solo se muestra una vez)
+            messages.success(request,f'Se agregó correctamente la region {region.id}')
+
+            return redirect('region')
+            #return HttpResponse(articulo.titulo + ' - ' + articulo.contenido + ' - ' + str(articulo.publicado))
+    else:
+        formulario = FormRegion()        
+    return render(request, 'crear_region.html',{
+        'form': formulario
+    })
 
 def empleados(request):
     return render(request,'empleados.html')
